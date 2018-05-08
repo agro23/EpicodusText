7f4b6915d45bca39d12c37b3fde88b82c4dc48e5
Andy Grossberg <andy.grossberg@gmail.com>
2018-05-08 09:12:41 -0700

    Add project files.

---
+++ EpicodusText/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EpicodusText
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
